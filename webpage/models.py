be933d2a6d54d1b766a09af4a0f1d10c32e6b5c3
Perceval <albertobustac@gmail.com>
2024-12-04 19:38:49 -0300

    fix BD precios

--- webpage/models.py
+++ webpage/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Plan(models.Model):
     nombre = models.CharField(max_length=100, unique=True)
     precio = models.PositiveIntegerField()
+    precio_anual = models.PositiveIntegerField()
 
     def __str__(self):
         return self.nombre
