8d007e53bf8473cdc14c0749de00d914fdd104e2
Perceval <albertobustac@gmail.com>
2024-12-04 19:15:32 -0300

    planes agregados a la DB

--- webpage/admin.py
+++ webpage/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Cliente, Plan, Estado, Pago
 
-# Register your models here.
+# Registramos los modelos en el sitio de administración
+admin.site.register(Cliente)
+admin.site.register(Plan)
+admin.site.register(Estado)
+admin.site.register(Pago)
